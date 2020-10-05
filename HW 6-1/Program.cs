1c4e7dc69765b1f3f95e5b30dccc348dd974c714
Upendra Samaranayake <humanfrailtz@gmail.com>
2020-10-05 15:01:11 -0700

    Add project files.

---
+++ HW 6-1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HW_6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] firstArray = new char[10] { 'x', 'g', 'z', 'd', 'b', 'n', 'f', 's', 'h', 'e' };
+            char[] secondArray = new char[firstArray.Length];
+
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                secondArray[i] = firstArray[i];
+            }
+
+            Console.WriteLine(secondArray);
+            Console.ReadLine();
+        }
+    }
+}
