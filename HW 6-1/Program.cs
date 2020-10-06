cc169b4f4ca24b8fd3bed80c041eaba5ac67d97d
Upendra Samaranayake <humanfrailtz@gmail.com>
2020-10-05 17:15:11 -0700

    Style Sheet

--- HW 6-1/Program.cs
+++ HW 6-1/Program.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿/// Homework No.__ Exercise No.__
+/// File Name:     HW 6-1.sln
+/// @author:       Student Name (First Last)
+/// Date:          Sept. 1, 2015
+///
+/// Problem Statement: (what you want the code to do)
+///    
+/// Overall Plan (step-by-step, how you want the code to make it happen):
+/// 1. Create first array.
+/// 2. Create second array.
+/// 3. Run for loop and insert values from first array into second array.
+/// 4. Write out the entries of the 2nd array.
+/// etc.
+
+using System;
 
 namespace HW_6_1
 {
